885c904436c60ba9463fffdf6a3942e26cf3bbb1
Matt Kocubinski <mkocubinski@gmail.com>
2015-05-02 11:01:38 -0400

    Initial commit.

---
+++ init.el
@@ -0,0 +1,26 @@
+;; User pack init file
+;;
+;; Use this file to initiate the pack configuration.
+;; See README for more information.
+
+;; Load bindings config
+(live-load-config-file "bindings.el")
+
+;; install el-get
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+
+(setq my-packages
+      (append '(evil)))
+
+(el-get 'sync my-packages)
+
+(evil-mode 1)
