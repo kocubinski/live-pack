c5f2fa15ef37342537ee821de76de4458843ecc5
Matt Kocubinski <mkocubinski@gmail.com>
2015-05-02 17:07:11 -0400

    Processing mode.

--- init.el
+++ init.el
@@ -16,11 +16,22 @@
     (goto-char (point-max))
     (eval-print-last-sexp)))
 
-(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+(add-to-list 'el-get-recipe-path (concat (live-pack-lib-dir) "recipes"))
 
 (setq my-packages
-      (append '(evil)))
+      (append '(evil
+                processing2-emacs)))
 
 (el-get 'sync my-packages)
 
 (evil-mode 1)
+
+(autoload 'processing-mode "processing-mode" "Processing mode" t)
+(add-to-list 'auto-mode-alist '("\\.pde$" . processing-mode))
+
+(setq processing-location "C:/binski/apps/processing-2.2.1/processing-java.exe")
+(setq processing-application-dir "C:/binski/apps/processing-2.2.1/processing.exe")
+(setq processing-sketchbook-dir "C:/Users/Matt/Documents/Processing")
+
+;; display
+(set-face-attribute 'default nil :font "Consolas-11")
