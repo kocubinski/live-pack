3d3d67d0d384560e33a46169960892160a017a58
Matt Kocubinski <mkocubinski@gmail.com>
2015-05-07 11:13:11 -0400

    Add AC stuff, need to do something with processing path.

--- init.el
+++ init.el
@@ -30,9 +30,20 @@
 (autoload 'processing-mode "processing-mode" "Processing mode" t)
 (add-to-list 'auto-mode-alist '("\\.pde$" . processing-mode))
 
-(setq processing-location "C:/binski/apps/processing-2.2.1/processing-java.exe")
-(setq processing-application-dir "C:/binski/apps/processing-2.2.1/processing.exe")
-(setq processing-sketchbook-dir "C:/Users/Matt/Documents/Processing")
+(setq processing-location "C:/dev/apps/processing-2.2.1/processing-java.exe")
+(setq processing-application-dir "C:/dev/apps/processing-2.2.1/processing.exe")
+(setq processing-sketchbook-dir "C:/Users/mkocubinski/Documents/Processing")
+
+(defun processing-mode-init ()
+  (make-local-variable 'ac-sources)
+  (setq ac-sources '(ac-source-dictionary ac-source-yasnippet ac-source-words-in-buffer))
+  (make-local-variable 'ac-user-dictionary)
+  (setq ac-user-dictionary (append processing-functions
+                                   processing-builtins
+                                   processing-constants)))
+
+(add-to-list 'ac-modes 'processing-mode)
+(add-hook 'processing-mode-hook 'processing-mode-init)
 
 (autoload 'processing-snippets-initialize "processing-snippets" nil nil nil)
 (eval-after-load 'yasnippet '(processing-snippets-initialize))
